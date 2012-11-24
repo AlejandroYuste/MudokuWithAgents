13b596fd6d8771abd393d11b1ec8af16c160eec8
Jandro <Jandro@130.230.146.115>
2012-11-24 11:46:25 +0200

    Class Leader

---
+++ MudokuAgentVersion/src/AgentLeader.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+
+public class AgentLeader implements Runnable{
+
+	Random random = new Random(System.nanoTime());
+	int agentId;
+	int agentType;
+	
+	Agent agent;
+	
+	
+	AgentLeader(Agent agent_, int agentId_, int agentType_)
+	{
+		agentId = agentId_;
+		agentType = agentType_;
+		agent = agent_;
+	}
+	
+	@Override
+	public void run() 
+	{
+		while(true)
+		{
+			try
+			{
+				if (!Agent.getConflictExists())
+				{
+					Agent.Leader(agentId, agentType);
+					Thread.sleep((random.nextInt(agent.getNumAgentsConnected()) + 1) * 250);
+				}
+				else Thread.sleep(10000);
+				
+			} catch (InterruptedException e) {
+				System.out.println("Error Produit al Run del Thread del Agent: " + agentId);
+				e.printStackTrace();
+			}
+		}
+	}
+}
